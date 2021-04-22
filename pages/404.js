3bb98fa9108f3aa8dac644943603652c417d7bd5
jasonandrew <jasonxandrewth@gmail.com>
2021-04-22 11:42:05 +0200

    fallback true

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,8 @@
 // pages/404.js
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+  return (
+    <div className="fixed top-0 left-0 flex w-screen h-screen justify-center items-center">
+      <h1>404 - Page Not Found</h1>
+    </div>
+  )
 }
